75e1bbc01e8ad42fa5d48573d688d492c88fd901
gonzavecchi <gonzavecchi@gmail.com>
2016-08-30 22:01:02 -0300

    Parte 2 del ejercicio 25 sin formato

--- DestinoEjercicio25.php
+++ DestinoEjercicio25.php
@@ -111,12 +111,15 @@ body {
 
 if (isset($_POST["LadoUno"]) && isset($_POST["LadoDos"])) { //CON ISSET VALIDO QUE LA VARIABLE NO ESTÉ VACÍA.
     $_superficie = $_POST["LadoUno"] * $_POST["LadoDos"];
-    echo "<br>$_superficie";
+    echo "<br>$_superficie<br><br>";
+
 }
 
 
 
  ?>
+ <a href="http://localhost:8080/ClaseTres/login.php" >Ir a la página anterior</a>
+ 
 </div>
 </body>
 
